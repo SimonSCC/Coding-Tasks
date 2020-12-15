d3f061e5d6b851acf5943cda28f5ab8add9d4508
Dragonslayer420 <simonsondergaardchristiansen@gmail.com>
2020-12-15 02:05:43 +0100

    two starred day 6

--- Adventofcode/Day6 - Custom Customs/Day6/Program.cs
+++ Adventofcode/Day6 - Custom Customs/Day6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day6
@@ -7,12 +8,79 @@ namespace Day6
     {
         static void Main(string[] args)
         {
-            var Lines = File.ReadAllLines("test.txt");
+            var Lines = File.ReadAllLines("input.txt");
             Part1(Lines);
-            Console.ReadKey();
-            Part2(Lines);
+            //Console.ReadKey();
+            Part2Attempt2(Lines);
 
 
+
+        }
+
+        private static void Part2Attempt2(string[] lines)
+        {
+            //end result should be 6 on test.txt
+            int sum = 0;
+            bool removed = false;
+
+            List<string> groups = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] != "") //Not new group
+                {
+                    groups.Add(lines[i]);
+                }
+                if (lines[i] == "" || i == lines.Length - 1)
+                {
+                    if (groups.Count == 1)
+                    {
+                        Console.WriteLine("Sum increased on line: " + i);
+                        sum += groups[0].Length;
+                        Console.WriteLine("Sum total: " + sum);
+                    }
+                    else
+                    {
+                        string firstString = groups[0];
+                        for (int u = 0; u < firstString.Length; u++)
+                        {
+                            char test = firstString[u];
+                            int amountRight = 0;
+                            if (!removed)
+                            {
+                                groups.RemoveAt(0);
+                                removed = true;
+
+                            }
+
+                            foreach (var items2 in groups)
+                            {
+                                if (items2.Contains(test)) //This solved it. Wow. I thought it had to be on the same index like code below
+                                {
+                                    amountRight++;
+                                }
+                                //if (u >= items2.Length)
+                                //{
+                                //    break;
+                                //}
+                                //if (items2[u] == test)
+                                //{
+                                //    amountRight++;
+                                //}
+                            }
+                            if (amountRight == groups.Count)
+                            {
+                                Console.WriteLine("Sum increased on line: " + i);
+                                sum++;
+                                Console.WriteLine("Sum total: " + sum);
+                            }                           
+                        }
+                        removed = false;
+                    }
+                    groups = new List<string>();
+                }
+            }
+            Console.WriteLine(sum);
 
         }
 
